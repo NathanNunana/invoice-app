53dad9130bd7bb2ff0d5fff0a13617c055cf21f0
Nathan Nunana Kulewoshie <55038849+NathanNunana@users.noreply.github.com>
2023-04-23 15:51:13 +0000

    button components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
-import { Invoices, ViewInvoice } from './pages';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Invoices, ViewInvoice } from "./pages";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Invoices />} />
-        <Route path='/view-invoice'  element={<ViewInvoice />}/>
+        <Route path="/" element={<Invoices />} />
+        <Route path="/view-invoice" element={<ViewInvoice />} />
       </Routes>
     </div>
   );
